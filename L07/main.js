84f860479a2193aafc118353d55d99df27e4dfe0
Z3r0cks <patrick.kaserer@web.de>
2020-12-01 18:57:10 +0100

    fix: l07

--- L07/main.js
+++ L07/main.js
@@ -1,12 +1,10 @@
 var L07;
 (function (L07) {
-    var wrapperClass = document.createElement("div");
     var playBtn = document.createElement('img');
-    wrapperClass.className = "drumPad";
     playBtn.src = './images/play-button.png';
     playBtn.className = "playBtn";
-    document.body.appendChild(wrapperClass);
     var drumPadArray = [
+        // innerer Index        0                                1                   2            //außerer Index
         [document.createElement("button"), new Audio('./assets/A.mp3'), "1f63d1"],
         [document.createElement("button"), new Audio('./assets/C.mp3'), "1f63d1"],
         [document.createElement("button"), new Audio('./assets/F.mp3'), "1f63d1"],
@@ -15,28 +13,28 @@ var L07;
         [document.createElement("button"), new Audio('./assets/kick.mp3'), "F52692"],
         [document.createElement("button"), new Audio('./assets/snare.mp3'), "F52692"],
         [document.createElement("button"), new Audio('./assets/laugh-1.mp3'), "CD700A"],
-        [document.createElement("button"), new Audio('./assets/laugh-2.mp3'), "CD700A"]
+        [document.createElement("button"), new Audio('./assets/laugh-2.mp3'), "CD700A"] // 8 
     ];
     var _loop_1 = function (i) {
         drumPadArray[i][0].className = "singlePad";
         drumPadArray[i][0].setAttribute('style', 'background-color: #' + drumPadArray[i][2]);
         drumPadArray[i][0].addEventListener('click', function () { return playSample((drumPadArray[i][1])); });
-        wrapperClass.appendChild(drumPadArray[i][0]);
+        document.getElementById("app").appendChild(drumPadArray[i][0]);
     };
     for (var i = 0; i < drumPadArray.length; i++) {
         _loop_1(i);
     }
-    wrapperClass.appendChild(playBtn);
+    document.getElementById("app").appendChild(playBtn);
     playBtn.addEventListener('click', function () {
         setInterval(function () {
             playSample(drumPadArray[5][1]);
             setTimeout(function () {
                 playSample(drumPadArray[6][1]);
-            }, 300);
+            }, 400);
             setTimeout(function () {
                 playSample(drumPadArray[4][1]);
-            }, 600);
-        }, 900);
+            }, 800);
+        }, 1200);
     });
     function playSample(audio) {
         audio.play();
--- L07/main.ts
+++ L07/main.ts
@@ -1,45 +1,44 @@
 namespace L07 {
-   const wrapperClass: HTMLDivElement = document.createElement("div")
    const playBtn: HTMLImageElement = document.createElement('img');
-   wrapperClass.className = "drumPad";
    playBtn.src = './images/play-button.png';
    playBtn.className = "playBtn";
-   document.body.appendChild(wrapperClass);
 
    const drumPadArray: (HTMLButtonElement | HTMLAudioElement | string)[][] = [
-      [document.createElement("button"), new Audio('./assets/A.mp3'), "1f63d1"],
-      [document.createElement("button"), new Audio('./assets/C.mp3'), "1f63d1"],
-      [document.createElement("button"), new Audio('./assets/F.mp3'), "1f63d1"],
-      [document.createElement("button"), new Audio('./assets/G.mp3'), "F52692"],
-      [document.createElement("button"), new Audio('./assets/hihat.mp3'), "F52692"],
-      [document.createElement("button"), new Audio('./assets/kick.mp3'), "F52692"],
-      [document.createElement("button"), new Audio('./assets/snare.mp3'), "F52692"],
-      [document.createElement("button"), new Audio('./assets/laugh-1.mp3'), "CD700A"],
-      [document.createElement("button"), new Audio('./assets/laugh-2.mp3'), "CD700A"]
+      // innerer Index        0                                1                   2            //außerer Index
+               [document.createElement("button"), new Audio('./assets/A.mp3'), "1f63d1"],             // 0
+               [document.createElement("button"), new Audio('./assets/C.mp3'), "1f63d1"],             // 1
+               [document.createElement("button"), new Audio('./assets/F.mp3'), "1f63d1"],             // 2
+               [document.createElement("button"), new Audio('./assets/G.mp3'), "F52692"],             // 3
+               [document.createElement("button"), new Audio('./assets/hihat.mp3'), "F52692"],         // 4
+               [document.createElement("button"), new Audio('./assets/kick.mp3'), "F52692"],          // 5
+               [document.createElement("button"), new Audio('./assets/snare.mp3'), "F52692"],         // 6
+               [document.createElement("button"), new Audio('./assets/laugh-1.mp3'), "CD700A"],       // 7
+               [document.createElement("button"), new Audio('./assets/laugh-2.mp3'), "CD700A"]        // 8 
    ]
 
    for (let i = 0; i < drumPadArray.length; i++) {
       (drumPadArray[i][0] as HTMLButtonElement).className = "singlePad";
       (drumPadArray[i][0] as HTMLButtonElement).setAttribute('style', 'background-color: #' + drumPadArray[i][2]);
       (drumPadArray[i][0] as HTMLButtonElement).addEventListener('click', () => playSample((drumPadArray[i][1]) as HTMLAudioElement))
-      wrapperClass.appendChild((drumPadArray[i][0] as HTMLButtonElement));
+      document.getElementById("app").appendChild((drumPadArray[i][0] as HTMLButtonElement));
    }
-   wrapperClass.appendChild(playBtn);
+
+   document.getElementById("app").appendChild(playBtn);
 
    playBtn.addEventListener('click', () => {
       setInterval(() => {
          playSample((drumPadArray[5][1] as HTMLAudioElement))
          setTimeout(() => {
             playSample((drumPadArray[6][1] as HTMLAudioElement))
-         }, 300);
+         }, 400);
          setTimeout(() => {
             playSample((drumPadArray[4][1] as HTMLAudioElement))
-         }, 600);
-
-      }, 900)
+         }, 800);
+      }, 1200)
    })
 
    function playSample(audio: HTMLAudioElement) {
       audio.play();
    }
+
 }
